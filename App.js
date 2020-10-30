68b113a0048dea4142a29a87161bd1d5e16a69b3
Shadowgamer483 <60564162+Shadowgamer483@users.noreply.github.com>
2020-10-30 21:18:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Login_Screen from './screens/login'
+export default function App() {
+  return (
+   <Login_Screen />
+
+
+
+
+
+   
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
